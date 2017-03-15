59afbb0865514d69653efc20c0abd2f83e2b46a3
dav11d <s1aven@msn.com>
2017-03-15 23:06:20 +0000

    api

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@
 use Mix.Config
 
 
-config :darksky, forecast_io_api_key: "68768c4c6a4409930b51f3b48b6bfccb"
+config :darksky, forecast_io_api_key: "your-api-key"
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
